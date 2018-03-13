c3e7f6e5cd24a1dd06301a3ca2eef4b6820709c4
sdsz666 <lion.singing@gmail.com>
2018-03-14 01:13:38 +0800

    paixu

--- src/com/ymt/apiTest/testBubbleSort.java
+++ src/com/ymt/apiTest/testBubbleSort.java
@@ -4,7 +4,7 @@ public class testBubbleSort {
 
     public static void BubbleSort(){
         int temp;
-        int[] array  =new int[]{33,23,44,52,11,6,24,57};
+        int[] array  ={33,23,44,52,11,6,24,57};
 
         for (int i = 0;i <array.length;i ++){
             for(int j=0;j<array.length-1;j++){
@@ -27,7 +27,7 @@ public class testBubbleSort {
     public static void SelectSort(){
         int max;
         int temp;
-        int[] array  =new int[]{33,23,44,52,11,6,24,57};
+        int[] array  ={33,23,44,52,11,6,24,57};
         for (int i=0;i<array.length;i++){
             max=array[i];
             for(int j=array.length-1;j>i;j--){
@@ -41,8 +41,35 @@ public class testBubbleSort {
         }
     }
 
+    public static void InsertSort(){
+        int[] array  ={1,2,5,6,7,8,9,3,10};
+        int count=0;
+        int insertNum;
+        for (int i=1;i<array.length;i++){
+            insertNum=array[i];
+            for (int j=i-1;j>=0;j--){
+                //前面的大于后面的，则调换，即小的排在前面
+                if (array[j]>insertNum){
+                    array[j+1]=array[j];
+                    count++;
+                    System.out.println("排序次数+1");
+                }else {
+                    break;
+                }
+                array[j]=insertNum;
+            }
+        }
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println("排序次数为"+count);
+
+    }
+
+
     public static void main(String args[]){
-        BubbleSort();
+        //BubbleSort();
+        InsertSort();
 
     }
 
